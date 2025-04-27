ed02bef160ae7130452eb81cafc91aadd5b5e08d
Matheus Garcia <matheusggarcia.v@gmail.com>
2025-04-27 13:41:31 -0300

    Niver

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const birthdayImage = document.querySelector('.birthday-image');
+
+    birthdayImage.addEventListener('click', () => {
+        alert('Feliz Aniversário! Que todos os seus desejos se realizem!');
+    });
+});
